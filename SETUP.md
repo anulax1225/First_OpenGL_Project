3a4305af94bd356851f34669c1130092dd314c08
anulax1225 <anulax1225@icloud.com>
2023-09-30 12:31:00 +0200

    Setup of OpenGL and first program

---
+++ SETUP.md
@@ -0,0 +1,47 @@
+# Install dependencies :
+
+* g++ the C++ compileur
+``` bash
+
+	sudo apt install g++
+
+```
+ 
+* cmake and xorg-dev to build GLFW
+``` bash
+
+	sudo apt install build-essential cmake xorg-dev
+
+```
+
+* Adds other dependencies
+```bash 
+
+	sudo apt-get install libx11-dev libglu1-mesa-dev freeglut3-dev libglew2.2 libglew-dev libglu1-mesa libgl1-mesa-glx libgl1-mesa-dev libglfw3-dev libglfw3
+
+```
+
+# Installing GLFW :
+
+Install source package for OpenGL from glfw.org/download.html
+
+Go to the GLFW folder and write commandes :
+```bash
+
+	cmake -G "Unix Makefiles"
+	make
+	sudo make install  
+```
+
+# Install GLAD :
+Install glad zip from the website (check local files at the end of the page)
+https://glad.dav1d.de/
+Add glad to your project directory
+
+# Compile :
+``` bash
+
+	g++ src/*.cpp include/glad/glad.c -o bin/prog -std=c++11 -Wall -lGL -lGLU -lglut -lGLEW -lglfw -lX11 -lXxf86vm -lXrandr -lpthread -lXi -ldl -lXinerama -lXcursor
+
+
+```
---
+++ src/main.cpp
@@ -0,0 +1,61 @@
+#include "../include/glad/glad.h"
+#include <GLFW/glfw3.h>
+
+#include <iostream>
+
+void FrameBufferSizeCallback(GLFWwindow* window, int width, int height);
+
+int main() {
+    //Const variables
+    const int WIDTH = 1000;
+    const int HEIGHT = 800;
+    //Init of middleware
+    glfwInit();
+    //Defines the middleware for OpenGL
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    //Only defined for Apple
+#ifdef __APPLE__
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+#endif
+
+    //Creation of the window
+    GLFWwindow* window = glfwCreateWindow(WIDTH,HEIGHT,"First OpenGL window", NULL, NULL);
+    //Verifing that the window was correctly created else ends the program
+    if (window == NULL) {
+        std::cout << "Could not open the window.\n";
+        // if couldn't open the window terminate glfw window and ends the program
+        glfwTerminate();
+        return -1;
+    }
+    //Sets focus on the current window
+    glfwMakeContextCurrent(window);
+    //Initializes GLAD and verifies that it didn't fail
+    if(!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+        std::cout << "Failed to initialize GLAD.\n";
+        // if glad failed terminate glfw window and ends the program
+        glfwTerminate();
+        return -1; 
+    }
+    //Sets the size of the view port
+    //The view port says to OpenGL how to render thing depending on the screen size
+    glViewport( 0, 0, WIDTH, HEIGHT);
+    //Sets behavior when window is resized
+    glfwSetFramebufferSizeCallback(window, FrameBufferSizeCallback);
+    //Main loop
+    while(!glfwWindowShouldClose(window)) {
+        //Swaps the image buffer for the next one
+        glfwSwapBuffers(window);
+        // Computing next frame while the last one is rendered
+        glfwPollEvents();
+    } 
+    //Once finished termenate the window
+    glfwTerminate();
+    return 0;
+}
+
+//Resizes the view port when user changes the screen size
+void FrameBufferSizeCallback(GLFWwindow* window, int width, int height) {
+    glViewport(0, 0, width, height);
+}
